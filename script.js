8f866166fe6bdd32f7cfb01e5503bc7d133be562
PatilSandesh-07 <sandesh.876731@gmail.com>
2025-09-07 22:09:13 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+
+const menuBtn = document.getElementById("menu-btn");
+const navLinks = document.querySelector(".nav-links");
+
+menuBtn.addEventListener("click", () => {
+  navLinks.classList.toggle("active");
+});
